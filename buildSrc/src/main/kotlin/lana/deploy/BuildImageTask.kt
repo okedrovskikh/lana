73bc0d822711b80494d1c5ad7b7fc6cd6178ad7b
o.kedrovskikh <sololasuklina@gmail.com>
2023-10-17 23:12:17 +0500

    Add docker images pushing

---
+++ buildSrc/src/main/kotlin/lana/deploy/BuildImageTask.kt
@@ -0,0 +1,33 @@
+package lana.deploy
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import java.io.InputStreamReader
+import java.io.SequenceInputStream
+import java.util.concurrent.Executors
+
+abstract class BuildImageTask : DefaultTask() {
+    private val coroutinePool = Executors.newFixedThreadPool(5)
+
+    @get:Input
+    open var version: String = "latest"
+
+    @get:Input
+    abstract var imageName: String
+
+    @get:Input
+    open var dockerfilePath: String? = null
+
+    @TaskAction
+    fun execute() {
+        val baseDockerCommand = "docker build -t $imageName:$version"
+        val command = TerminalCommand(if (dockerfilePath == null) { baseDockerCommand } else { "$baseDockerCommand $dockerfilePath" })
+        val proc = command.runCommand()
+        val out = InputStreamReader(SequenceInputStream(proc.inputStream, proc.errorStream))
+        coroutinePool.submit { out.forEachLine { println(it) } }
+        val termCode = proc.waitFor()
+        coroutinePool.shutdown()
+        if (termCode != 0) error("Not 0 termination code")
+    }
+}
---
+++ buildSrc/src/main/kotlin/lana/deploy/PublishDockerImageToLocalMinikube.kt
@@ -0,0 +1,28 @@
+package lana.deploy
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import java.io.InputStreamReader
+import java.io.SequenceInputStream
+import java.util.concurrent.Executors
+
+abstract class PublishDockerImageToLocalMinikube : DefaultTask() {
+    private val coroutinePool = Executors.newFixedThreadPool(5)
+
+    @get:Input
+    abstract var imageName: String
+
+    @get:Input
+    open var version: String = "latest"
+
+    @TaskAction
+    fun execute() {
+        val proc = TerminalCommand("minikube image load $imageName:$version").runCommand()
+        val out = InputStreamReader(SequenceInputStream(proc.inputStream, proc.errorStream))
+        coroutinePool.submit { out.forEachLine { println(it) } }
+        val termCode = proc.waitFor()
+        coroutinePool.shutdown()
+        if (termCode != 0) error("Not 0 termination code")
+    }
+}
